20be10a3562afafa64f1497531809617eacc32d5
Lukil <prigioneluca33@gmail.com>
2025-10-16 22:07:14 +0200

    Start Parentesizzazione

---
+++ Prog_1/02-La_chiamata_del_destino/Start_file/start_parentesizzazione.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define N 100
+
+char parentesi[N] = {'(',')','(','(','(',')',')',')',')','(','\0'};
+
+int main(){
+    return 0;
+}
---
+++ Prog_1/02-La_chiamata_del_destino/Start_file/start_parentesizzazione.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#define N 100
+
+char parentesi[N] = {'(',')','(','(','(',')',')',')',')','(','\0'};
+
+int main(){
+    return 0;
+}
