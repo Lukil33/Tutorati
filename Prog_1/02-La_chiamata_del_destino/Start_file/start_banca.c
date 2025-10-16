ab89aa0109e57a9edc9e57e00e3082a4f3749b20
Lukil <prigioneluca33@gmail.com>
2025-10-16 21:30:56 +0200

    Start file banca dati

---
+++ Prog_1/02-La_chiamata_del_destino/Start_file/start_banca.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define N 100
+
+int banca_dati[N] = {'\0'};
+
+void aggiungere(int* banca, int num);
+
+void rimuovere(int* banca, int num);
+
+int cercare(int* banca, int num);
+
+void stampare(int* banca);
+
+int main(){
+    return 0;
+}
---
+++ Prog_1/02-La_chiamata_del_destino/Start_file/start_banca.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define N 100
+
+int banca_dati[N] = {'\0'};
+
+void aggiungere(int* banca, int num);
+
+void rimuovere(int* banca, int num);
+
+int cercare(int* banca, int num);
+
+void stampare(int* banca);
+
+int main(){
+    return 0;
+}
